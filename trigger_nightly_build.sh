3f1ec8a413eaf920eb7404ef90b6459e2e361585
Tsubasa Nomura <tsubasa0820@hotmail.com>
2017-01-14 18:03:41 +0000

    Relase1:Update

---
+++ trigger_nightly_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+_project=$1
+_branch=$2
+_circle_token=$3
+
+trigger_build_url=https://circleci.com/api/v1/project/${_project}/tree/${_branch}?circle-token=${_circle_token}
+
+
+curl \
+    --header "Accept: application/json" \
+    --header "Content-Type: application/json" \
+    --request POST ${trigger_build_url}
