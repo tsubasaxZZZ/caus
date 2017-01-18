ffd5abc88f70a96f31be999078b97a92405121b7
Tsubasa Nomura <tsubasa0820@hotmail.com>
2017-01-18 04:59:29 +0000

    Update caus.py

--- caus.py
+++ caus.py
@@ -7,7 +7,9 @@ from bs4 import BeautifulSoup
 import pymongo
 from pymongo import MongoClient
 from datetime import datetime
-from libs.common import DROP_DOWN_ID, parse_drop_downlist, SERVICE_UPDATE_BASEURL, AZURE_BASEURL
+from libs.common import DROP_DOWN_ID, Util, SERVICE_UPDATE_BASEURL, AZURE_BASEURL, CAUS_LOGGER
+from pprint import pformat
+
 
 #DROP_DOWN_ID = {"product" : "dropdown-products", "updateType" : "dropdown-updatetype"}
 MONGODB_HOST = "aus-db"
@@ -27,9 +29,8 @@ def get_update_list(**condition):
     update_list = []
 
     #div_serviceupdate_list = soup.find_all("div", attrs={"class" : "serviceUpdate"})
-    div_serviceupdate_list = soup.select('#top > div.wa-container > section > div.row.serviceUpdates-container > div.column > div.row.row-size1')
+    div_serviceupdate_list = soup.select('#top > main.wa-container > section > div.row.serviceUpdates-container > div.column > div.row.row-size1')
     for div_serviceupdate in div_serviceupdate_list:
-
         update_item = {}
         update_item['title'] = div_serviceupdate.h5.text
         update_item['url'] = div_serviceupdate.a['href']
@@ -37,12 +38,13 @@ def get_update_list(**condition):
         # サマリの取得
         summary_html = requests.get(AZURE_BASEURL + "/" + update_item['url']).text
         summary_soup = BeautifulSoup(summary_html, "html.parser")
-        date_html = summary_soup.select('#top > div.wa-container > section.section > div.row > div.column > p')[0].text
+        date_html = summary_soup.select('#top > main.wa-container > section.section > div.row > div.column > p')[0].text
         update_item['date'] = datetime.strptime(date_html,"%A, %B %d, %Y")
 #        update_item['summary'] = ''.join([text.text for text in summary_soup.select('#top > div.wa-container > section.section > div.row.row-size3 > div.column.small-12.medium-9 > p')])
-#        update_item['summary'] = summary_soup.select('#top > div.wa-container > section.section > div.row.row-size3 > div.column > p')
+#        update_item['summary'] = summary_soup.select('#top > main.wa-container > section.section > div.row.row-size3 > div.column.small-12.medium-9 > p')
         update_item['summary'] = ''
         update_item['condition'] = [condition]
+        CAUS_LOGGER.debug(update_item)
         update_list.append(update_item)
 
     return update_list
@@ -51,19 +53,18 @@ def create_updates_db(db):
     '''
     MongoDB へインサート
     '''
-    dropdown_list = parse_drop_downlist()
-
+    util = Util(CAUS_LOGGER)
+    dropdown_list = util.get_drop_downlist()
+    
     for dropdown_type, dropdown_value in DROP_DOWN_ID.items():
         for _key, _val in dropdown_list[dropdown_value].items():
 
-            print("============== {}:{}:{} ===========".format(dropdown_type, _key, _val))
-
+            CAUS_LOGGER.debug("*** DROPDOWN_TYPE=[{}], DROPDOWN_LIST_KEY=[{}], DROPDOWN_LIST_VAL=[{}] ***".format(dropdown_type, _key, _val))
             for page_num in range(1, MAX_PAGING):
                 update_list = get_update_list(**dict({dropdown_type:_val, "page":page_num}))
                 if(len(update_list) == 0):
                     break
                 for update in update_list:
-                    print(update)
                     try:
                         db['update_temp1'].insert(update)
                     except Exception as e:
